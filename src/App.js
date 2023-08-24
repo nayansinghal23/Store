3f434be58635a1706e1cb3227776f077f49b1a8e
Nayan <nayansinghal393@gmail.com>
2023-08-24 21:58:12 +0530

    Code Splitting

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import "./App.css";
-import Products from "./components/Products";
 import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
-import SelectedItems from "./components/SelectedItems";
+import Loading from "./components/Loading";
+
+const Products = lazy(() => import("./components/Products"));
+const SelectedItems = lazy(() => import("./components/SelectedItems"));
 
 const App = () => {
   return (
     <Provider store={store}>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Products />} />
-        <Route path="/selectedItems" element={<SelectedItems />} />
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<Products />} />
+          <Route path="/selectedItems" element={<SelectedItems />} />
+        </Routes>
+      </Suspense>
     </Provider>
   );
 };
---
+++ src/components/Loading.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <h1
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      Loading...
+    </h1>
+  );
+};
+
+export default Loading;
